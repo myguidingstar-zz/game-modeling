fec71279b486f2c5ca9816251cb55293c2d8e06e
Hoàng Minh Thắng <p@banphim.net>
2013-12-12 23:22:06 +0700

    add a sample world

--- core.scm
+++ core.scm
@@ -14,3 +14,11 @@
         [(equal? 'X sym)
          "blue"]
         [else "white"]))
+
+;; A sample 4x4 world should look like this
+(list
+ ;; cols 0 1 2 3  /  rows
+ (vector O O O _) ;   0
+ (vector _ X O X) ;   1
+ (vector O O _ _) ;   2
+ (vector O X O _));   3
