38d3fd07ae395170601bf8e4c6fb91185969c427
Hoàng Minh Thắng <p@banphim.net>
2013-12-12 23:21:32 +0700

    define some quick symbols

---
+++ core.scm
@@ -0,0 +1,9 @@
+#lang racket
+
+;; faster naming so that sample input can be typed with ease
+;; These names' values are symbols (instead of color strings)
+;; so that they can be printed with same look.
+
+(define O 'O)
+(define X 'X)
+(define _ '_)
